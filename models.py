d1e7d0a79f1cb5ba2fd17c5d65958d1ee4f99dc9
wartorle <wartorle@student.21-school.ru>
2025-10-18 12:37:44 +0300

    add models

---
+++ models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    firmvare_version = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    batteries = relationship("Battery", back_populates="device")
+
+class Battery(Base):
+    __tablename__ = "batteries"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
