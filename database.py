c65102f6be7e82f0d2394179c742bd09be1b5c2a
wartorle <wartorle@student.21-school.ru>
2025-10-18 12:50:51 +0300

    edit

--- database.py
+++ database.py
@@ -7,5 +7,9 @@ DATABASE_URL = "sqlite:///./batteries.db"
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+#Создаём фабрику сессий
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+#Родитель для всех моделей
 Base = declarative_base()
